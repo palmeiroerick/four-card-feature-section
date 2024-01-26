321e0230bd7835d594c5d675125b62cd83287394
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-26 18:42:45 -0300

    style: add desktop styles to title component

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -4,7 +4,7 @@ type TitleProps = {
 
 const Title = ({ children }: TitleProps) => {
   return (
-    <h1 className="text-center text-2xl font-semibold text-veryDarkBlue">
+    <h1 className="text-center text-2xl font-semibold text-veryDarkBlue lg:text-4xl">
       {children}
     </h1>
   );

8ad28f921d2016dc70e0dea1f7501c5cc01a0355
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-26 18:42:02 -0300

    style: add desktop styles to home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Title from "@/components/Title";
 
 const Home = () => {
   return (
-    <main className="w-80 flex flex-col gap-16 my-20">
-      <section className="flex flex-col gap-4">
+    <main className="my-20 flex w-80 flex-col gap-16 lg:w-[1080px] lg:items-center lg:gap-14 lg:m-0">
+      <section className="flex flex-col gap-4 lg:w-[520px]">
         <div className="flex flex-col-reverse gap-1">
           <Title>Powered by Technology</Title>
           <SubTitle>Reliable, efficient delivery</SubTitle>
@@ -16,8 +16,9 @@ const Home = () => {
           points to ensure that your project is successful
         </Text>
       </section>
-      <section className="flex flex-col gap-6">
+      <section className="flex flex-col gap-6 lg:grid lg:grid-cols-3 lg:grid-rows-2 lg:items-center lg:gap-8">
         <Card
+          className="lg:row-start-1 lg:row-end-3"
           id="Supervisor"
           text="Monitors activity to identify project roadblocks"
         />
@@ -30,6 +31,7 @@ const Home = () => {
           text="Regularly evaluates our talent to ensure quality"
         />
         <Card
+          className="lg:col-start-3 lg:col-end-4 lg:row-start-1 lg:row-end-3"
           id="Calculator"
           text="Uses data from past projects to provide better delivery estimates"
         />

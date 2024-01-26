b02524afc1c8b378243893979a66d608bf9bbaf6
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-26 18:14:30 -0300

    style: add mobile styles to card component

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -6,6 +6,13 @@ type CardProps = {
 };
 
 const Card = ({ id, text }: CardProps) => {
+  const borderColor = {
+    Supervisor: "border-t-cyan",
+    "Team Builder": "border-t-red",
+    Karma: "border-t-orange",
+    Calculator: "border-t-blue",
+  };
+
   const iconUrl = {
     Supervisor: "/assets/icon-supervisor.svg",
     "Team Builder": "/assets/icon-team-builder.svg",
@@ -14,12 +21,14 @@ const Card = ({ id, text }: CardProps) => {
   };
 
   return (
-    <div>
-      <div>
-        <h3>{id}</h3>
-        <p>{text}</p>
+    <div
+      className={`${borderColor[id]} flex justify-between h-56 w-80 flex-col rounded-md border-t-4 p-7 shadow-[0_6px_16px] shadow-grayishBlue`}
+    >
+      <div className="flex flex-col gap-2">
+        <h3 className="text-xl text-veryDarkBlue font-semibold">{id}</h3>
+        <p className="text-[13px] text-grayishBlue">{text}</p>
       </div>
-      <Image src={iconUrl[id]} alt="" width={48} height={48} />
+      <Image className="self-end" src={iconUrl[id]} alt="" width={48} height={48} />
     </div>
   );
 };
